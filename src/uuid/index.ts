1f5ff70d6d1fc1c95520a619de5db5707caed0e3
liutsing <liutsingluo@gmail.com>
2022-06-07 14:49:20 +0800

    feat: add uuid implemetation

---
+++ src/uuid/index.ts
@@ -0,0 +1,20 @@
+export function uuidv4() {
+  // @ts-ignore FIXME
+  const seed = [1e7] + -1e3 + -4e3 + -8e3 + -1e11; // 10000000-1000-4000-8000-100000000000
+  return seed.replace(/[018]/g, (c) =>
+    (
+      c ^
+      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+    ).toString(16)
+  );
+}
+
+export function uuid() {
+  let d = new Date().getTime();
+  const uuid = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
+    const r = (d + Math.random() * 16) % 16 | 0;
+    d = Math.floor(d / 16);
+    return (c === "x" ? r : (r & 0x7) | 0x8).toString(16);
+  });
+  return uuid;
+}
